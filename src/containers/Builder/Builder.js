3e99350b6a935a332aea89ae07f20e0ca49c18ad
Ryan Moore <rhcmoore@gmail.com>
2019-02-28 12:58:57 -0800

    Implement build adder

--- src/containers/Builder/Builder.js
+++ src/containers/Builder/Builder.js
@@ -3,6 +3,13 @@ import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
+const INGREDIENT_PRICES = {
+    salad: 0.3,
+    bacon: 0.7,
+    cheese: 0.5,
+    meat: 1.3
+}
+
 class Builder extends React.Component {
     state = {
         ingredients: {
@@ -10,14 +17,36 @@ class Builder extends React.Component {
             bacon: 0,
             cheese: 1,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        // grab old Count from state and increment
+        const updatedCount = this.state.ingredients[type] + 1;
+        // make a copy of ingredients an update relevant
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        // updated total Price = old price + price addition
+        const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+        // set state with new totalPrice and ingredients
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    }
+
+    removeIngredientHandler = (type) => {
+
     }
 
     render() {
         return (
             <Aux>
-                <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                />
             </Aux>
         )
     }
