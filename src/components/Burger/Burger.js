a26f30b73ef73fc15c63329e744ec79b1989caa2
Ryan Moore <rhcmoore@gmail.com>
2019-02-27 16:49:48 -0800

    Refactor ingredients mapping

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,21 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
     // keys passed as ingredients
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingredientKey => {
             // returning BurgerIngredient components corresponding with keys & value #s
             return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
                 return <BurgerIngredient key={ingredientKey+i} type={ingredientKey} />
             });
-        });
-    
+            // flattening array to check for null
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add ingredients</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
