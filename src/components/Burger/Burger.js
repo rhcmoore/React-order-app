02dd0521435c616a88b3f853e8e99380eb7d2351
Ryan Moore <rhcmoore@gmail.com>
2019-02-27 16:44:44 -0800

    Pass and render ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+    // keys passed as ingredients
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingredientKey => {
+            // returning BurgerIngredient components corresponding with keys & value #s
+            return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+                return <BurgerIngredient key={ingredientKey+i} type={ingredientKey} />
+            });
+        });
+    
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
