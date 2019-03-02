d21f51ace73f5db7b1346812efc0194671207ba4
Ryan Moore <rhcmoore@gmail.com>
2019-03-01 18:05:50 -0800

    Add lifecycle methods to Modal to prevent unnecessary updates

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,31 +3,37 @@ import Aux from "../../../hoc/Aux";
 import Button from "../../UI/Button/Button"
 
 // for modal
-const orderSummary = (props) => {
+class OrderSummary extends React.Component {
 
-    // transform object to array
-    const ingredientSummary = Object.keys(props.ingredients).map(ingKey => {
+    componentWillUpdate() {
+        console.log("[Order Summary] willUpdate");
+    }
+
+    render() {
+        // transform object to array
+        const ingredientSummary = Object.keys(this.props.ingredients).map(ingKey => {
+            return (
+                <li key={ingKey}>
+                    <span style={{ textTransform: "capitalize" }}>{ingKey}</span>
+                    : {this.props.ingredients[ingKey]}
+                </li>
+            )
+        })
+
         return (
-            <li key={ingKey}>
-                <span style={ {textTransform: "capitalize"} }>{ingKey}</span>
-                : {props.ingredients[ingKey]}
-            </li>
+            <Aux>
+                <h3>Your Order</h3>
+                <p>Your order includes:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Checkout</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
+            </Aux>
         )
-    })
-
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>Your order includes:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Checkout</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
-        </Aux>
-    )
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,18 +3,32 @@ import classes from "./Modal.css";
 import Aux from "../../../hoc/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div className={classes.Modal}
-            // modal handling
-            style={{
-                transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-                opacity: props.show ? "1" : "0"
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends React.Component {
+    // controlling update of OrderSummary
+    shouldComponentUpdate(nextProps, nextState) {
+        // update only when show changes
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    componentWillUpdate() {
+        console.log("[Modal] willUpdate")
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div className={classes.Modal}
+                    // modal handling
+                    style={{
+                        transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+                        opacity: this.props.show ? "1" : "0"
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+
+export default Modal;
