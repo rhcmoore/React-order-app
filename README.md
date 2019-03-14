4959cc437150c746665c400da6a0db9ff32bc080
Ryan Moore <rhcmoore@gmail.com>
2019-03-14 13:31:23 -0700

    Update form with two-way binding

--- README.md
+++ README.md
@@ -5,16 +5,3 @@ Basic web order interface for In-N-Out burgers. Built with React and with mobile
 Experimenting with React design patterns and responsiveness. 
 
 <img src="./README.png" width="100%">
-
-### Layout and Components
-                            [App]
-
-                            [Layout]
-
-    [Toolbar]------------ [Side Drawer] ------------ [Backdrop] -------------[{props.children}]
-
-    [DrawerToggle]                                                      [Different Pages]
-                                                                         [Order Builder]
-    [---------Logo--------------------]                 [Build Controls] ----------- [Item] ----------- [Modal]
-    [---------Navigation Items--------]              [Controls...] [Order]   [Item ingredients...]  [{props.children}]
-                                                           
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,14 +3,61 @@ import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.css";
 import axios from "../../../axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
+import Input from "../../../components/UI/Input/Input";
 
 class ContactData extends React.Component {
     state = {
-        name: "",
-        email: "",
-        address: {
-            street: "",
-            zipCode: ""
+        orderForm: {
+            name: {
+                elementType: "input",
+                elementConfig: {
+                    type: "text",
+                    placeholder: "Name"
+                },
+                value: ""
+            },
+            street: {
+                elementType: "input",
+                elementConfig: {
+                    type: "text",
+                    placeholder: "Street"
+                },
+                value: ""
+            },
+            zipCode: {
+                elementType: "input",
+                elementConfig: {
+                    type: "text",
+                    placeholder: "Zip Code"
+                },
+                value: ""
+            },
+            country: {
+                elementType: "input",
+                elementConfig: {
+                    type: "text",
+                    placeholder: "Country"
+                },
+                value: ""
+            },
+            email: {
+                elementType: "input",
+                elementConfig: {
+                    type: "text",
+                    placeholder: "Email"
+                },
+                value: ""
+            },
+            deliveryMethod: {
+                elementType: "select",
+                elementConfig: {
+                    options: [
+                        { value: "fastest", displayValue: "Fastest" },
+                        { value: "cheapest", displayValue: "Cheapest" }
+                    ]
+                },
+                value: ""
+            },
         },
         loading: false
     }
@@ -21,17 +68,6 @@ class ContactData extends React.Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price, // would need to calculate on server-side in prod env
-            // hard-coded, will update with auth data
-            customer: {
-                name: "Rick Berry",
-                address: {
-                    street: "Lombard",
-                    zipCode: "41414",
-                    country: "USA"
-                },
-                email: "charlie@brown.edu"
-            },
-            deliveryMethod: "fastest"
         }
         // post order to db
         axios.post("/orders.json", order) // .json is Firebase notation
@@ -44,13 +80,40 @@ class ContactData extends React.Component {
             });
     }
 
+    inputChangedHandler = (event, inputId) => {
+        // clone order form from state
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+        // deep clone of value like email/name
+        const updatedFormElement = {
+            ...updatedOrderForm[inputId]
+        }
+        // change value of select element 
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputId] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm})
+    }
+
     render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-                <input className={classes.Input} type="email" name="email" placeholder="yours@email.com" />
-                <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                <input className={classes.Input} type="text" name="zipCode" placeholder="Zip Code" />
+                {formElementsArray.map(formEl => (
+                    <Input
+                        key={formEl.id}
+                        elementType={formEl.config.elementType}
+                        elementConfig={formEl.config.elementConfig}
+                        value={formEl.config.value} 
+                        changed={(event) => this.inputChangedHandler(event, formEl.id)}/>
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>Submit</Button>
             </form>
         );
