1445ac9fd1797848cc6979bbca479cd6f1f49f9c
Ryan Moore <rhcmoore@gmail.com>
2019-03-14 14:04:52 -0700

    Add validation message

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,10 +3,17 @@ import classes from "./Input.css";
 
 const input = (props) => {
     let inputElement = null;
+    let validationError = null;
     const inputClasses = [classes.InputElement];
+
     // only marked invalid if it has validation checkers and has been touched
     if (props.invalid && props.shouldValidate && props.touched) {
         inputClasses.push(classes.Invalid);
+    }
+
+    // Error message
+    if (props.invalid && props.touched) {
+        validationError = <p className={classes.ValidationError}>Please enter a valid value</p>;
     }
 
     switch (props.elementType) {
@@ -50,6 +57,7 @@ const input = (props) => {
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     );
 };
