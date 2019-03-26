10c51d6fb206cd41f9603e49248f71f6d9f2fdfe
Ryan Moore <rhcmoore@gmail.com>
2019-03-26 14:05:06 -0700

    Updates

---
+++ precache-manifest.a8154423f4e49034fac8b7660c27daa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "808e049c5532eb61f144",
+    "url": "/React-order-app/static/css/main.4436e4b6.chunk.css"
+  },
+  {
+    "revision": "808e049c5532eb61f144",
+    "url": "/React-order-app/static/js/main.808e049c.chunk.js"
+  },
+  {
+    "revision": "6111b7a36903e329113f",
+    "url": "/React-order-app/static/js/runtime~main.6111b7a3.js"
+  },
+  {
+    "revision": "a83ba603738dc692c05b",
+    "url": "/React-order-app/static/js/2.a83ba603.chunk.js"
+  },
+  {
+    "revision": "3c0533ee7a9849d9982cf95eda7ca93f",
+    "url": "/React-order-app/static/media/logo.3c0533ee.png"
+  },
+  {
+    "revision": "5bdaec80f3ac34a37d1d2565610bfceb",
+    "url": "/React-order-app/index.html"
+  }
+];
